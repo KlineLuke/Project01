fb42f8bf6c87e3f86f0232046e82123497128731
KlineLuke <36570713+KlineLuke@users.noreply.github.com>
2018-02-24 13:46:05 -0500

    added comments

--- BubbleSort.h
+++ BubbleSort.h
@@ -14,15 +14,19 @@ class BubbleSort{
 public:
     virtual bool needSwap(PetDatabaseSortable* sortableVector, int i, int j) const = 0;
     
-    void sort(PetDatabaseSortable* sortableVector){
+    void sort(PetDatabaseSortable* sortableVector)
+    {
         bool sorted = false;
         int n=sortableVector->getSize();
-        while( !sorted ){
+        while( !sorted )
+        {
             sorted = true;
-            for(int i=1; i<n; i++){
-                if(needSwap(sortableVector, i-1, i)){
-                    sortableVector->swap(i-1,i);
-                    sorted = false;
+            for(int i=1; i<n; i++)  // loop through vector n times
+            {
+                if(needSwap(sortableVector, i-1, i))    // check for swap
+                {
+                    sortableVector->swap(i-1,i);        // call swap
+                    sorted = false;      // change to false, not finished sorting
                 }
             }
             n--;

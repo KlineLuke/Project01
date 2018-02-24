80b64f1dda9675f89af4d02a335013584258dac0
KlineLuke <36570713+KlineLuke@users.noreply.github.com>
2018-02-24 13:56:01 -0500

    added comments

--- PetDatabaseSortable.h
+++ PetDatabaseSortable.h
@@ -1,6 +1,6 @@
 /* 
  * File:   PetDatabaseSortable.h
- * Author: klineluk
+ * Author: opperm27/klineluk
  *
  * Created on February 21, 2018, 10:59 AM
  */
@@ -19,31 +19,40 @@ class PetDatabaseSortable {
 protected:
     vector <Pet*> m_PetVector;
 public:  
-    PetDatabaseSortable() {}
+    PetDatabaseSortable() {} // default constructor
+    // constructor
     PetDatabaseSortable( vector<Pet*> pets ): m_PetVector(pets) {}
     
-    virtual unsigned int getSize() const {
+    virtual unsigned int getSize() const 
+    {
         return m_PetVector.size();
     }
     
+    // virtual function for all the sortables
     virtual bool smaller(int i, int j) const = 0;
     
-    virtual void swap(int i, int j){
+    // swaps pets at position i and j
+    virtual void swap(int i, int j)
+    {
         Pet* temp = m_PetVector[i];
         m_PetVector[i]=m_PetVector[j];
         m_PetVector[j]=temp;
     }
     
-    virtual void DisplayRecords() const {
+    // prints out all of the pets in the vector
+    virtual void DisplayRecords() const 
+    {
         for(int i=0; i<m_PetVector.size(); i++)
             m_PetVector[i]->print();
     }
     
-    Pet* getPet(int i) const{
+    virtual Pet* getPet(int i) const
+    {
         return m_PetVector[i];
     }
     
-    void insert(Pet* element) {
+    void insert(Pet* element) 
+    {
         m_PetVector.push_back(element);
     }
     
@@ -52,7 +61,6 @@ public:
         return m_PetVector;
     }
 };
-
 
 #endif	/* PETDATABASESORTABLE_H */
 

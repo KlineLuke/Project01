9f7626beeab926331de877db01baf79de640ae61
KlineLuke <36570713+KlineLuke@users.noreply.github.com>
2018-02-24 13:40:55 -0500

    added comments

--- BinarySearch.h
+++ BinarySearch.h
@@ -8,6 +8,7 @@
 #ifndef BINARYSEARCH_H
 #define	BINARYSEARCH_H
 
+//#include "PetDatabaseSortable.h"
 #include "PetDatabaseSearchable.h"
 
 class BinarySearch {
@@ -15,17 +16,19 @@ public:
     int search(PetDatabaseSearchable* vec) {
         int left = 0;
         int right = vec->getSize();
-        
-        while (left <= right) {
+
+        while (left <= right) 
+        {
             int middle = (left + right) / 2;
+            
             if(vec->CompareAt(middle) == 0)
-                return middle;
+                return middle;      // search succeeded
             else if (vec->CompareAt(middle) == 1)
                 right = middle - 1;
             else
                 left = middle + 1;
         }
-        return -1;
+        return -1;                  // search failed
     }
 
 }; 

f557a899dcba44b873a970b35cc831fb39ac9b4b
KlineLuke <36570713+KlineLuke@users.noreply.github.com>
2018-02-24 15:16:51 -0500

    removed petvector

--- PetDatabaseSortableByType.h
+++ PetDatabaseSortableByType.h
@@ -15,8 +15,6 @@
 
 class PetDatabaseSortableByType: public PetDatabaseSortable
 {
-    protected:
-        vector<Pet*> m_PetVector;
     public:
         PetDatabaseSortableByType( vector<Pet*> pets) : PetDatabaseSortable(pets) {}
         virtual bool smaller(int i, int j) const 
